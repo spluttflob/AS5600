49e06dadffc5c1778d6b03a0cfbb0764c2078ba5
J Ridge <60046013+spluttflob@users.noreply.github.com>
2020-12-21 21:54:46 -0800

    Began modification to allow multiple AS5600's in a system

--- AS5600.cpp
+++ AS5600.cpp
@@ -1,77 +1,113 @@
-#include "Arduino.h"
+/** @file AS5600.cpp
+ *    This is a library for interacting with the AS5600 magnetic angle sensor.
+ *    It has been forked from the original by Kane C. Stoboi and modified so
+ *    that it can be used to communicate wtih more than one AS5600. 
+ *  @author Created by Kane C. Stoboi, 22 August, 2018, and released into the 
+ *          public domain.
+ *  @author Forked by JR Ridgely
+ *  @date   2020-Dec-21 Forked by JRR
+ *  @copyright Additions by JRR are copyright (c) 2020 by JR Ridgely and
+ *             released under the Lesser GNU Public License, version 3.0. 
+ */
+
 #include "AS5600.h"
 
-AS5600::AS5600()
-{
-  //set up AS5600
-  Wire.begin();
 
-}
-long AS5600::getPosition()
+/** @brief   Construct a driver for one AS5600 sensor.
+ *  @details This constructor sets up a driver for an AS5600. 
+ *  @param   p_i2c_bus A pointer to the I2C driver (such as @c Wire) being used
+ *  @param   i2c_address The address of this AS5600 on its I2C bus
+ */
+AS5600::AS5600 (TwoWire* p_i2c_bus, uint8_t i2c_address)
 {
-  return _getRegisters2(_RAWANGLEAddressMSB, _RAWANGLEAddressLSB);  
-}
-
-int AS5600::getAngle()
-{
-  return _getRegisters2(_ANGLEAddressMSB, _ANGLEAddressLSB);  
+    _p_I2C = p_i2c_bus;
+    _i2c_addr = i2c_address;
 }
 
-int AS5600::getStatus()
+
+long AS5600::getPosition (void)
 {
-  return _getRegister(_STATUSAddress) & 0b00111000;
+    return _getRegisters2 (_RAWANGLEAddressMSB, _RAWANGLEAddressLSB);  
 }
 
-int AS5600::getGain()
+
+int AS5600::getAngle (void)
 {
-  return _getRegister(_AGCAddress);
+    return _getRegisters2 (_ANGLEAddressMSB, _ANGLEAddressLSB);  
 }
 
-int AS5600::getMagnitude()
+
+int AS5600::getStatus (void)
 {
-  return _getRegisters2(_MAGNITUDEAddressMSB, _MAGNITUDEAddressLSB);
+    return _getRegister (_STATUSAddress) & 0b00111000;
 }
 
-int AS5600::_getRegister(byte register1)
+
+int AS5600::getGain (void)
 {
-  Wire.beginTransmission(_AS5600Address);
-  Wire.write(register1);
-  Wire.endTransmission();
-
-  Wire.requestFrom(_AS5600Address, 1);
-
-  if(Wire.available() <=1) {
-    _msb = Wire.read();
-  }
-
-  return _msb;
+    return _getRegister (_AGCAddress);
 }
 
-long AS5600::_getRegisters2(byte registerMSB, byte registerLSB)
+int AS5600::getMagnitude (void)
 {
-  _lsb = 0;
-  _msb = 0;
-  
-  Wire.beginTransmission(_AS5600Address);
-  Wire.write(registerMSB);
-  Wire.endTransmission();
-  delay(10);
+    return _getRegisters2 (_MAGNITUDEAddressMSB, _MAGNITUDEAddressLSB);
+}
 
-  Wire.requestFrom(_AS5600Address, 1);
 
-  if(Wire.available() <=1) {
-    _msb = Wire.read();
-  }
+int AS5600::_getRegister (uint8_t register1)
+{
+    _p_I2C->beginTransmission (_i2c_addr);
+    _p_I2C->write (register1);
+    _p_I2C->endTransmission ();
 
-  Wire.requestFrom(_AS5600Address, 1);
+    _p_I2C->requestFrom (_i2c_addr, 1);
 
-  Wire.beginTransmission(_AS5600Address);
-  Wire.write(registerLSB);
-  Wire.endTransmission();
+    if (_p_I2C->available () <= 1)
+    {
+        _msb = _p_I2C->read ();
+    }
 
-  if(Wire.available() <=1) {
-    _lsb = Wire.read();
-  }
+    return _msb;
+}
 
-  return (_lsb) + (_msb & _msbMask) * 256;
+
+long AS5600::_getRegisters2 (byte registerMSB, byte registerLSB)
+{
+    _lsb = 0;
+    _msb = 0;
+
+    _p_I2C->beginTransmission (_i2c_addr);
+    _p_I2C->write (registerMSB);
+    _p_I2C->endTransmission ();
+    // #ifdef FreeRTOS
+    //     vTaskDelay (10);
+    // #else
+    //     delay (10);                                   //////////////////////////////////
+    // #endif
+
+    _p_I2C->requestFrom (_i2c_addr, 1);
+
+    if (_p_I2C->available () <= 1) 
+    {
+        _msb = _p_I2C->read ();
+    }
+
+    _p_I2C->requestFrom (_i2c_addr, 1);
+
+    _p_I2C->beginTransmission (_i2c_addr);
+    _p_I2C->write (registerLSB);
+    _p_I2C->endTransmission ();
+
+    if (_p_I2C->available () <= 1) 
+    {
+        _lsb = _p_I2C->read ();
+    }
+
+    return (_lsb) + (_msb & _msbMask) * 256;
+}
+
+
+void setI2CAddress (uint8_t new_address)
+{
+
 }
--- AS5600.h
+++ AS5600.h
@@ -1,8 +1,14 @@
-/*
-  AS5600.h - Library for interacting with the AS5600.
-  Created by Kane C. Stoboi, 22 August, 2018.
-  Released into the public domain.
-*/
+/** @file AS5600.h
+ *    This is a library for interacting with the AS5600 magnetic angle sensor.
+ *    It has been forked from the original by Kane C. Stoboi and modified so
+ *    that it can be used to communicate wtih more than one AS5600. 
+ *  @author Created by Kane C. Stoboi, 22 August, 2018, and released into the 
+ *          public domain.
+ *  @author Forked by JR Ridgely
+ *  @date   2020-Dec-21 Forked by JRR
+ *  @copyright Additions by JRR are copyright (c) 2020 by JR Ridgely and
+ *             released under the Lesser GNU Public License, version 3.0. 
+ */
 
 #ifndef AS5600_h
 #define AS5600_h
@@ -10,46 +16,54 @@
 #include "Arduino.h"
 #include <Wire.h>
 
+
+/** @brief   Class which operates an AS5600 magnetic angle sensor.
+ */
 class AS5600
 {
-  public:
-    AS5600();
-    long getPosition();
-    int getAngle();
-    int getStatus();
-    int getGain();
-    int getMagnitude();
-    void setZero();
-    
-    
-    private: 
-      int _AS5600Address = 0x36;
-      
-      byte _ZMCOAddress = 0x00;
-      byte _ZPOSAddressMSB = 0x01;
-      byte _ZPOSAddressLSB = 0x02;
-      byte _MPOSAddressMSB = 0x03;
-      byte _MPOSAddressLSB = 0x04;
-      byte _MANGAddressMSB = 0x05;
-      byte _MANGAddressLSB = 0x06;
-      byte _CONFAddressMSB = 0x07;
-      byte _CONFAddressLSB = 0x08;
-      byte _RAWANGLEAddressMSB = 0x0C;
-      byte _RAWANGLEAddressLSB = 0x0D;
-      byte _ANGLEAddressMSB = 0x0E;
-      byte _ANGLEAddressLSB = 0x0F;
-      byte _STATUSAddress = 0x0B;
-      byte _AGCAddress = 0x1A;
-      byte _MAGNITUDEAddressMSB = 0x1B;
-      byte _MAGNITUDEAddressLSB = 0x1C;
-      byte _BURNAddress = 0xFF;
+public:
+    AS5600 (TwoWire* p_i2c_bus, uint8_t i2c_address);
+    long getPosition (void);
+    int getAngle (void);
+    int getStatus (void);
+    int getGain (void);
+    int getMagnitude (void);
+    void setZero (void);
+    void setI2CAddress (uint8_t new_address);
 
-      long _msb;
-      long _lsb;
-      long _msbMask = 0b00001111;
+protected: 
+    const uint8_t _ZMCOAddress = 0x00;
+    const uint8_t _ZPOSAddressMSB = 0x01;
+    const uint8_t _ZPOSAddressLSB = 0x02;
+    const uint8_t _MPOSAddressMSB = 0x03;
+    const uint8_t _MPOSAddressLSB = 0x04;
+    const uint8_t _MANGAddressMSB = 0x05;
+    const uint8_t _MANGAddressLSB = 0x06;
+    const uint8_t _CONFAddressMSB = 0x07;
+    const uint8_t _CONFAddressLSB = 0x08;
+    const uint8_t _RAWANGLEAddressMSB = 0x0C;
+    const uint8_t _RAWANGLEAddressLSB = 0x0D;
+    const uint8_t _ANGLEAddressMSB = 0x0E;
+    const uint8_t _ANGLEAddressLSB = 0x0F;
+    const uint8_t _STATUSAddress = 0x0B;
+    const uint8_t _AGCAddress = 0x1A;
+    const uint8_t _MAGNITUDEAddressMSB = 0x1B;
+    const uint8_t _MAGNITUDEAddressLSB = 0x1C;
+    const uint8_t _BURNAddress = 0xFF;
 
-      long _getRegisters2(byte registerMSB, byte registerLSB);
-      int _getRegister(byte register1);
+    long _msb;
+    long _lsb;
+    long _msbMask = 0b00001111;
+
+    /// A pointer to the I2C driver which will be used for communication
+    TwoWire* _pI2C;
+
+    /// The I2C address of this particular sensor
+    uint8_t _i2c_addr;
+
+    long _getRegisters2 (uint8_t registerMSB, uint8_t registerLSB);
+    int _getRegister (uint8_t register1);
 };
 
-#endif
+#endif // AS5600_h
+
